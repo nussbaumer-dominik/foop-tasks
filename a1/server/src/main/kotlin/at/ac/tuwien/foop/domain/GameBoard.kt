1d431c82b43ccfcb6ee88f978be812580a50b015
Dominik Nussbaumer <nussbaumer.dominik@pm.me>
2023-06-25 13:30:51 +0200

    fixed merge conflicts and deleted getFieldAtPosition and grid from the GameBoard

--- a1/server/src/main/kotlin/at/ac/tuwien/foop/domain/GameBoard.kt
+++ a1/server/src/main/kotlin/at/ac/tuwien/foop/domain/GameBoard.kt
@@ -1,7 +1,6 @@
 package at.ac.tuwien.foop.domain
 
 import at.ac.tuwien.foop.common.domain.GameBoardDto
-import at.ac.tuwien.foop.common.exceptions.IllegalPositionException
 
 /**
  * The structure of the current map of the current game
@@ -13,7 +12,6 @@ class GameBoard(
     val width: Int = 0,
     val height: Int = 0,
     var winningSubway: Subway? = null,
-    private var grid: Array<Array<Entity>>? = null, //TODO: remove it
     private var ways: Set<Way> = mutableSetOf()
 ) {
     companion object {
@@ -85,13 +83,6 @@ class GameBoard(
         }
     }
 
-    fun getFieldAtPosition(position: Position): Entity {
-        if (position.x < 0 || position.x >= width || position.y < 0 || position.y >= height)
-            throw IllegalPositionException("Position is out of bounds")
-
-        return grid!![position.x][position.y]
-    }
-
     fun selectWinningSubway() {
         val random = (0 until subways.size).random()
         winningSubway = subways.elementAt(random)
@@ -99,51 +90,6 @@ class GameBoard(
 
     fun getSubwayExitPairs(): List<Pair<Subway, Exit>> {
         return subways.flatMap { s -> s.exits.map { e -> Pair(s, e) } }
-    }
-
-    fun toDto(): GameBoardDto {
-        return GameBoardDto(
-            width = width,
-            height = height,
-            mice = mice.map { it.toDto() }.toMutableSet(),
-            subwayDtos = subways.map { it.toDto() }.toMutableSet(),
-            playerDtos = players.map { it.toDto() }.toMutableSet(),
-        )
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as GameBoard
-
-        if (subways != other.subways) return false
-        if (mice != other.mice) return false
-        if (players != other.players) return false
-        if (width != other.width) return false
-        if (height != other.height) return false
-        if (winningSubway != other.winningSubway) return false
-        if (grid != null) {
-            if (other.grid == null) return false
-            if (!grid.contentDeepEquals(other.grid)) return false
-        } else if (other.grid != null) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = subways.hashCode()
-        result = 31 * result + mice.hashCode()
-        result = 31 * result + players.hashCode()
-        result = 31 * result + width
-        result = 31 * result + height
-        result = 31 * result + (winningSubway?.hashCode() ?: 0)
-        result = 31 * result + (grid?.contentDeepHashCode() ?: 0)
-        return result
-    }
-
-    override fun toString(): String {
-        return "GameBoard(subways=$subways, mice=$mice, players=$players, width=$width, height=$height, winningSubway=$winningSubway, grid=${grid?.contentToString()})"
     }
 
     /**
@@ -311,10 +257,6 @@ class GameBoard(
         if (width != other.width) return false
         if (height != other.height) return false
         if (winningSubway != other.winningSubway) return false
-        if (grid != null) {
-            if (other.grid == null) return false
-            if (!grid.contentDeepEquals(other.grid)) return false
-        } else if (other.grid != null) return false
 
         return true
     }
@@ -326,11 +268,10 @@ class GameBoard(
         result = 31 * result + width
         result = 31 * result + height
         result = 31 * result + (winningSubway?.hashCode() ?: 0)
-        result = 31 * result + (grid?.contentDeepHashCode() ?: 0)
         return result
     }
 
     override fun toString(): String {
-        return "GameBoard(subways=$subways, mice=$mice, players=$players, width=$width, height=$height, winningSubway=$winningSubway, grid=${grid?.contentToString()})"
+        return "GameBoard(subways=$subways, mice=$mice, players=$players, width=$width, height=$height, winningSubway=$winningSubway)"
     }
 }
